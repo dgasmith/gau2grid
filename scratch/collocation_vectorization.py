ad6051c135eb69f3a63eebcc365a4b4b63752954
Daniel Smith <malorian@me.com>
2017-11-04 10:33:11 -0400

    Updates generator to include gradients

--- scratch/collocation_vectorization.py
+++ scratch/collocation_vectorization.py
@@ -4,70 +4,66 @@ NumPy based example project.
 
 import numpy as np
 
+names = ["X", "Y", "Z"]
 # Generator
 print("Order generator")
-L = 3
+spacer = "        "
+L = 1
+idx = 0
 for i in range(L + 1):
-    l = L - i
+    l = L - i + 1
     for j in range(i + 1):
-        m = i - j
-        n = j
-        print(l, m, n)
+        m = i - j + 1
+        n = j + 1
 
 
+        lp = l - 1;
+        mp = m - 1;
+        np = n - 1;
+        name = "X" * lp + "Y" * mp + "Z" * np
 
+        # Density 
+        print("# Basis %s" % name)
+        print("# Density")
 
-npoints = 10
-nprim = 3
-alpha = [5, 6, 7]
-norm = [0.25, 0.5, 0.25]
-L = 2
+        print(spacer + "xyz = np.einsum('p,p,p->p', xc_pow[%d], yc_pow[%d], zc_pow[%d])" % (l, m, n))
 
+        tmp = spacer
+        tmp += "np.einsum('p,p->p', S0, xyz, "
+        tmp += "out=phi_out[%d])" % idx
+        print(tmp)
 
-alpha *= -1
+        print("# Derivatives")
+        if lp > 0:
+            tmp = spacer
+            tmp += "np.einsum(',p,p,p,p->p', %d, S0, " % lp
+            tmp += "xc_pow[%d], " % lp
+            tmp += "yc_pow[%d], " % m
+            tmp += "zc_pow[%d], " % n
+            tmp += "out=phi_out_x[%d])" % idx
+            print(tmp)
+        print(spacer + "phi_out_x[%d] += SX * xyz" % idx)
 
-x = np.random.rand(npoints)
-y = np.random.rand(npoints)
-z = np.random.rand(npoints)
+        if mp > 0:
+            tmp = spacer
+            tmp += "np.einsum(',p,p,p,p->p', %d, S0, " % mp
+            tmp += "xc_pow[%d], " % mp
+            tmp += "yc_pow[%d], " % m
+            tmp += "zc_pow[%d], " % n
+            tmp += "out=phi_out_y[%d])" % idx
+            print(tmp)
+        print(spacer + "phi_out_y[%d] += SY * xyz" % idx)
+    
+        if np > 0:
+            tmp = spacer
+            tmp += "np.einsum(',p,p,p,p->p', %d, S0, " % np
+            tmp += "xc_pow[%d], " % np
+            tmp += "yc_pow[%d], " % m
+            tmp += "zc_pow[%d], " % n
+            tmp += "out=phi_out_z[%d])" % idx
+            print(tmp)
+        print(spacer + "phi_out_z[%d] += SZ * xyz" % idx)
+        print(" ")
 
-R2 = x * x + y * y + z * z
+        idx += 1
 
-S0 = np.zeros(npoints)
-for K in range(nprim):
-    S0 += norm[K] * np.exp(- alpha[K] * R2)
-
-# SX, SY, SZ, SXX, SXZ, SXZ, SYY, SYZ, SZZ
-
-xc_pow = np.zeros(L, npoints)
-yc_pow = np.zeros(L, npoints)
-zc_pow = np.zeros(L, npoints)
-
-xc_pow[0] = 1.0
-yc_pow[0] = 1.0
-zc_pow[0] = 1.0
-
-for LL in range(1, L + 1):
-    xc_pow[LL] = xc_pow[LL - 1] * xc
-    yc_pow[LL] = xc_pow[LL - 1] * yc
-    zc_pow[LL] = xc_pow[LL - 1] * zc
-
-output = np.zeros((LL * 2 + 1, npoints))
-
-# S 
-output[0] = S0 * xc_pow[0] * yc_pow[0] * zc_pow[0]
-
-# P
-output[0] = S0 * xc_pow[1] * yc_pow[0] * zc_pow[0]
-output[1] = S0 * xc_pow[0] * yc_pow[1] * zc_pow[0]
-output[2] = S0 * xc_pow[0] * yc_pow[0] * zc_pow[1]
-
-# D
-output[0] = S0 * xc_pow[2] * xc_pow[0] * xc_pow[0]
-output[1] = S0 * xc_pow[1] * xc_pow[1] * xc_pow[0]
-output[2] = S0 * xc_pow[1] * xc_pow[0] * xc_pow[1]
-output[3] = S0 * xc_pow[0] * xc_pow[2] * xc_pow[0]
-output[4] = S0 * xc_pow[0] * xc_pow[1] * xc_pow[1]
-output[5] = S0 * xc_pow[0] * xc_pow[0] * xc_pow[2]
-
-
-# ... 
