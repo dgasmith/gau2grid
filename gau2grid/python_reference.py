514947d38586fc46b4e67a8e0fb52ba84199cbe3
Daniel Smith <malorian@me.com>
2017-11-06 14:15:48 -0500

    First python reference in gau2grid

--- gau2grid/python_reference.py
+++ gau2grid/python_reference.py
@@ -1,50 +1,136 @@
 import numpy as np
 
-def compute_shell(x, y, z, shell, grad=0):
 
-    alpha = shell["exp"]
-    L = shell["am"]
-    norm = shell["coef"]
-    center = shell["center"]
-    nprim = len(norm)
-    npoints = x.size
+def compute_collocation(xyz, L, coeffs, exponents, center, grad=0):
+    """
+    Computes the collocation matrix for a given set of cartesian points and a contracted gaussian of the form:
+        \sum_i coeff_i e^(exponent_i * R^2)
 
 
-    xc = x - center[0]
-    yc = y - center[1]
-    zc = z - center[2]
+    Parameters
+    ----------
+    xyz : array_like
+        The (N, 3) cartesian points to compute the grid on
+    L : int
+        The angular momentum of the gaussian
+    coeffs : array_like
+        The coefficients of the gaussian
+    exponents : array_like
+        The exponents of the gaussian
+    center : array_like
+        The cartesian center of the gaussian
+
+
+    """
+
+    # Unpack the shell data
+    nprim = len(coeffs)
+    npoints = xyz.shape[0]
+
+    # First compute the diff distance in each cartesian
+    xc = xyz[:, 0] - center[0]
+    yc = xyz[:, 1] - center[1]
+    zc = xyz[:, 2] - center[2]
     R2 = xc * xc + yc * yc + zc * zc
 
-    S0 = np.zeros(npoints)
+    # Build up the derivates in each direction
+    V1 = np.zeros((npoints))
+    V2 = np.zeros((npoints))
+    V3 = np.zeros((npoints))
     for K in range(nprim):
-        S0 += norm[K] * np.exp(- alpha[K] * R2)
+        T1 = coeffs[K] * np.exp(-exponents[K] * R2)
+        T2 = -2.0 * exponents[K] * T1
+        T3 = -2.0 * exponents[K] * T2
+        V1 += T1
+        V2 += T2
+        V3 += T3
+
+    S = V1.copy()
+    SX = V2 * xc
+    SY = V2 * yc
+    SZ = V2 * zc
+    SXY = V3 * xc * yc
+    SXZ = V3 * xc * zc
+    SYZ = V3 * yc * zc
+    SXX = V3 * xc * xc + V2
+    SYY = V3 * yc * yc + V2
+    SZZ = V3 * zc * zc + V2
 
     # SX, SY, SZ, SXX, SXZ, SXZ, SYY, SYZ, SZZ
 
-    xc_pow = np.zeros((L + 1, npoints))
-    yc_pow = np.zeros((L + 1, npoints))
-    zc_pow = np.zeros((L + 1, npoints))
+    # Power matrix for higher angular momenta
+    xc_pow = np.zeros((L + 3, npoints))
+    yc_pow = np.zeros((L + 3, npoints))
+    zc_pow = np.zeros((L + 3, npoints))
 
-    xc_pow[0] = 1.0
-    yc_pow[0] = 1.0
-    zc_pow[0] = 1.0
+    xc_pow[0] = 0.0
+    yc_pow[0] = 0.0
+    zc_pow[0] = 0.0
+    xc_pow[1] = 0.0
+    yc_pow[1] = 0.0
+    zc_pow[1] = 0.0
+    xc_pow[2] = 1.0
+    yc_pow[2] = 1.0
+    zc_pow[2] = 1.0
 
-    for LL in range(1, L + 1):
+    for LL in range(3, L + 3):
         xc_pow[LL] = xc_pow[LL - 1] * xc
         yc_pow[LL] = yc_pow[LL - 1] * yc
         zc_pow[LL] = zc_pow[LL - 1] * zc
 
+    # Allocate data
     ncart = int((L + 1) * (L + 2) / 2)
     output = {}
     output["PHI"] = np.zeros((ncart, npoints))
+    if grad > 0:
+        output["PHI_X"] = np.zeros((ncart, npoints))
+        output["PHI_Y"] = np.zeros((ncart, npoints))
+        output["PHI_Z"] = np.zeros((ncart, npoints))
+    if grad > 1:
+        output["PHI_XX"] = np.zeros((ncart, npoints))
+        output["PHI_YY"] = np.zeros((ncart, npoints))
+        output["PHI_ZZ"] = np.zeros((ncart, npoints))
+        output["PHI_XY"] = np.zeros((ncart, npoints))
+        output["PHI_XZ"] = np.zeros((ncart, npoints))
+        output["PHI_YZ"] = np.zeros((ncart, npoints))
+    if grad > 2:
+        raise ValueError("Only grid derivatives through Hessians (grad = 2) has been implemented")
 
+    # Loop over grid ordering data
     idx = 0
     for i in range(L + 1):
-        l = L - i
+        l = L - i + 2
         for j in range(i + 1):
-            m = i - j
-            n = j
-            output["PHI"][idx] = S0 * xc_pow[l] * yc_pow[m] * zc_pow[n]
+            m = i - j + 2
+            n = j + 2
+
+            a_lp = l - 2
+            a_mp = m - 2
+            a_np = n - 2
+
+            A = xc_pow[l] * yc_pow[m] * zc_pow[n]
+            AX = a_lp * xc_pow[l - 1] * yc_pow[m] * zc_pow[n]
+            AY = a_mp * xc_pow[l] * yc_pow[m - 1] * zc_pow[n]
+            AZ = a_np * xc_pow[l] * yc_pow[m] * zc_pow[n - 1]
+
+            output["PHI"][idx] = S * A
+            if grad > 0:
+                output["PHI_X"][idx] = S * AX + SX * A
+                output["PHI_Y"][idx] = S * AY + SY * A
+                output["PHI_Z"][idx] = S * AZ + SZ * A
+            if grad > 1:
+                AXY = a_lp * a_mp * xc_pow[l - 1] * yc_pow[m - 1] * zc_pow[n]
+                AXZ = a_lp * a_np * xc_pow[l - 1] * yc_pow[m] * zc_pow[n - 1]
+                AYZ = a_mp * a_np * xc_pow[l] * yc_pow[m - 1] * zc_pow[n - 1]
+                AXX = a_lp * (a_lp - 1) * xc_pow[l - 2] * yc_pow[m] * zc_pow[n]
+                AYY = a_mp * (a_mp - 1) * xc_pow[l] * yc_pow[m - 2] * zc_pow[n]
+                AZZ = a_np * (a_np - 1) * xc_pow[l] * yc_pow[m] * zc_pow[n - 2]
+                output["PHI_XX"][idx] = SXX * A + SX * AX + SX * AX + S * AXX
+                output["PHI_YY"][idx] = SYY * A + SY * AY + SY * AY + S * AYY
+                output["PHI_ZZ"][idx] = SZZ * A + SZ * AZ + SZ * AZ + S * AZZ
+                output["PHI_XY"][idx] = SXY * A + SX * AY + SY * AX + S * AXY
+                output["PHI_XZ"][idx] = SXZ * A + SX * AZ + SZ * AX + S * AXZ
+                output["PHI_YZ"][idx] = SYZ * A + SY * AZ + SZ * AY + S * AYZ
             idx += 1
 
     return output
