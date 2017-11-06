5ca35e5aca40a15d1d44632c2a2e8a5688841b63
Daniel Smith <malorian@me.com>
2017-11-06 14:33:37 -0500

    Adds first setup.py

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # gau2grid
 A collocation grid code
+
+## Installation
+To do a local install of the Python directory,
+```
+pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name='gau2grid',
+        version="0.1",
+        description='Fast computation of a gaussian and its derivate on a grid.',
+        author='Daniel G. A. Smith',
+        author_email='dgasmith@icloud.com',
+        url="https://github.com/dgasmith/gau2grid",
+        license='BSD-3C',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'numpy>=1.7',
+        ],
+        extras_require={
+            'docs': [
+                'sphinx==1.2.3',  # autodoc was broken in 1.3.1
+                'sphinxcontrib-napoleon',
+                'sphinx_rtd_theme',
+                'numpydoc',
+            ],
+            'tests': [
+                'pytest',
+                'pytest-cov',
+                'pytest-pep8',
+            ],
+        },
+
+        tests_require=[
+            'pytest',
+            'pytest-cov',
+            'pytest-pep8',
+        ],
+
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+        ],
+        zip_safe=True,
+    )
