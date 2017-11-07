3034988704827430b35b74119992afd373e1e84d
Daniel Smith <malorian@me.com>
2017-11-07 18:42:33 -0500

    Adds SciPy to reqs

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
         packages=setuptools.find_packages(),
         install_requires=[
             'numpy>=1.7',
+            'scipy>=0.18',
         ],
         extras_require={
             'docs': [
