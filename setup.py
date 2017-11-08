244af483da3116c1f08b737dac55e4832b69aa50
Daniel Smith <malorian@me.com>
2017-11-08 17:56:25 -0500

    CI: Switches out scipy for mpmath

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         packages=setuptools.find_packages(),
         install_requires=[
             'numpy>=1.7',
-            'scipy>=0.18',
+            'mpmath>=0.18',
         ],
         extras_require={
             'docs': [
