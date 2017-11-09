ea6ad9d11b460b474b0f4b62008fdc7589845c95
Daniel Smith <malorian@me.com>
2017-11-08 20:30:16 -0500

    CG: Starts an easy codegen file

--- gau2grid/__init__.py
+++ gau2grid/__init__.py
@@ -6,3 +6,4 @@ from . import np_generator as np_gen
 from . import python_reference as ref
 from . import RSH
 from . import order
+from . import codegen
---
+++ gau2grid/codegen.py
@@ -0,0 +1,33 @@
+"""
+Contains a simple codegen helper
+"""
+
+
+class CodeGen(object):
+    def __init__(self, indent="    "):
+        self.indent_lvl = 0
+        self.indent_tab = indent
+        self.data = []
+
+    def indent(self, lvl=1):
+        """
+        Indents the code one or more levels
+        """
+
+        self.indent_lvl += lvl
+
+    def dedent(self, lvl=1)
+        """
+        Dedents the code one or more levels
+        """
+
+        self.indent_lvl 0= lvl
+        if self.indent_lvl < 0:
+            raise ValueError("Indent level is negative!")
+
+    def write(self, line):
+        shift = self.indent_lvl * self.indent_tab
+        self.data.append(shift + line)
+
+    def repr(self, combine="\n"):
+        return combine.join(self.data)
