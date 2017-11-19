e5f92c6b98f79b2099c19c059f0fc1e656d98c1c
Daniel Smith <malorian@me.com>
2017-11-19 17:17:34 -0500

    GEN: Tests that the library builds

--- tests/test_c_generator.py
+++ tests/test_c_generator.py
@@ -12,19 +12,22 @@ np.set_printoptions(linewidth=120, suppress=True)
 import ref_basis
 import test_helper as th
 
+# Simply test that the code runs for now
 c_gen_tests = []
-for AM in range(6):
+for AM in range(4):
     for grad in range(3):
         c_gen_tests.append((AM, grad))
 
 @pytest.mark.parametrize("AM,grad", c_gen_tests)
 def test_c_collocation_codgen(AM, grad):
-    # Simply test that it runs for now
     cg = gg.codegen.CodeGen(cgen=True)
     gg.c_gen.shell_c_generator(cg, AM, grad=grad)
 
 @pytest.mark.parametrize("AM", list(range(6)))
 def test_c_spherical_trans_codgen(AM):
-    # Simply test that it runs for now
     cg = gg.codegen.CodeGen(cgen=True)
     gg.RSH.transformation_c_generator(cg, AM, "row")
+
+def test_library_gen():
+    gg.c_gen.generate_c_gau2grid(4, path="/tmp")
+
