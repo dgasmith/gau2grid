a83bc361cfe64a306423949da89140f207da05f7
Daniel Smith <malorian@me.com>
2017-12-07 21:42:38 -0500

    GEN: Factor util into another folder

--- gau2grid/c_generator.py
+++ gau2grid/c_generator.py
@@ -9,6 +9,7 @@ from . import codegen
 from . import order
 from . import utility
 from . import c_pragma
+from . import c_util_generator as c_util
 
 _grad_indices = ["x", "y", "z"]
 _hess_indices = ["xx", "xy", "xz", "yy", "yz", "zz"]
@@ -94,12 +95,15 @@ def generate_c_gau2grid(max_L, path=".", cart_order="row", inner_block=64, do_cf
     gg_header.blankline()
 
     # Fast transformers
-    trans_sig = _naive_transpose(gg_spherical)
+    gg_header.write("// Fast transposers")
+    trans_sig = c_util.naive_transpose(gg_spherical)
     gg_header.write(trans_sig)
-    fast_trans_sig = _fast_transpose(gg_spherical, 8)
+    fast_trans_sig = c_util.fast_transpose(gg_spherical, 8)
     gg_header.write(fast_trans_sig)
     gg_header.blankline()
 
+    # Fast copiers
+    gg_header.write("// Fast segment copiers")
 
     # Loop over phi, grad, hess and build blocks for each
     helper_sigs = []
@@ -181,12 +185,12 @@ def generate_c_gau2grid(max_L, path=".", cart_order="row", inner_block=64, do_cf
     gg_pybind.blankline()
 
     # Call the execute functions
-    _pybind11_func(gg_pybind, "collocation_wrapper", 0, helper_sigs[0], max_L)
-    _pybind11_func(gg_pybind, "collocation_deriv1_wrapper", 1, helper_sigs[1], max_L)
-    _pybind11_func(gg_pybind, "collocation_deriv2_wrapper", 2, helper_sigs[2], max_L)
+    c_util.pybind11_func(gg_pybind, "collocation_wrapper", 0, helper_sigs[0], max_L)
+    c_util.pybind11_func(gg_pybind, "collocation_deriv1_wrapper", 1, helper_sigs[1], max_L)
+    c_util.pybind11_func(gg_pybind, "collocation_deriv2_wrapper", 2, helper_sigs[2], max_L)
 
-    tsig = _pybind11_transpose(gg_pybind, "gg_naive_transpose", "naive_transpose_wrapper")
-    tsig = _pybind11_transpose(gg_pybind, "gg_fast_transpose", "fast_transpose_wrapper")
+    tsig = c_util.pybind11_transpose(gg_pybind, "gg_naive_transpose", "naive_transpose_wrapper")
+    tsig = c_util.pybind11_transpose(gg_pybind, "gg_fast_transpose", "fast_transpose_wrapper")
 
     # Open up the pybind module
     gg_pybind.start_c_block("PYBIND11_MODULE(pygg_core, m)")
@@ -268,7 +272,7 @@ def shell_c_generator(cg, L, function_name="", grad=0, cart_order="row", inner_b
     else:
         # Allocate as single block on stack
         for tmp in S_cache_tmps:
-             cg.write("double %s[%d] __attribute__((aligned(64)))" % (tmp, inner_block))
+            cg.write("double %s[%d] __attribute__((aligned(64)))" % (tmp, inner_block))
         S_tmps = []
 
     cg.blankline()
@@ -511,6 +515,7 @@ def _malloc(name, size, dtype="double"):
     # return "%s*  %s = (%s*)malloc(%s * sizeof(%s))" % (dtype, name, dtype, str(size), dtype)
     return "%s* __restrict__ %s = (%s*)_mm_malloc(%s * sizeof(%s), 32)" % (dtype, name, dtype, str(size), dtype)
 
+
 def _block_malloc(cg, block_name, names, size, dtype="double"):
     tot_size = len(names) * size
     cg.write(_malloc(block_name, tot_size))
@@ -747,116 +752,6 @@ def _S_pow_tmps(cg, L, grad, inner_block):
         cg.blankline()
 
 
-def _pybind11_func(cg, name, grad, call_name, max_L):
-    """
-    A function that builds the PyBind11 wrapper functions
-    """
-
-    # Figure out what we need to add per deriv
-    deriv_indices = utility.get_deriv_indices(grad)
-
-    # Write out wrapper functions
-    sig = """void %s(int L, py::array_t<double> arr_xyz, py::array_t<double> arr_coeffs,
-py::array_t<double> arr_exponents, py::array_t<double> arr_center, bool spherical,
-py::array_t<double> arr_out""" % name
-
-    # Pad out deriv outputs
-    for cart in deriv_indices:
-        sig += ", py::array_t<double> arr_%s_out" % cart
-
-    sig += ")"
-    cg.start_c_block(sig)
-    cg.blankline()
-
-    # Grab the pointers
-    cg.write('// Grab array pointers')
-    cg.write('auto xyz = arr_xyz.unchecked<2>()')
-    cg.write('auto coeffs = arr_coeffs.unchecked<1>()')
-    cg.write('auto exponents = arr_exponents.unchecked<1>()')
-    cg.write('auto center = arr_center.unchecked<1>()')
-    cg.write('auto out = arr_out.mutable_unchecked<2>()')
-
-    # Pad out deriv pointers
-    for cart in deriv_indices:
-        cg.write("auto out_%s = arr_%s_out.mutable_unchecked<2>()" % (cart, cart))
-
-    cg.blankline()
-
-    # Run through checks
-    cg.write('// XYZ is of size 3')
-    cg.start_c_block('if (L > %d)' % max_L)
-    cg.write(
-        '    throw std::invalid_argument("Exceeded compiled angular momentum of %d. Please recompile with a higher angular momentum.\\n")'
-        % max_L)
-    cg.close_c_block()
-
-    cg.write('// XYZ is of size 3')
-    cg.start_c_block('if (arr_xyz.shape(0) != 3)')
-    cg.write('    throw std::length_error("Length of XYZ array must be (3, n).\\n")')
-    cg.close_c_block()
-    cg.blankline()
-
-    cg.write('// Coeff matches exponent shape')
-    cg.start_c_block('if (coeffs.shape(0) != exponents.shape(0))')
-    cg.write('    throw std::length_error("Length of coefficients and exponents must match.\\n")')
-    cg.close_c_block()
-    cg.blankline()
-
-    cg.write('// Center is of size 3')
-    cg.start_c_block('if (center.shape(0) != 3)')
-    cg.write('    throw std::length_error("Length of center vector must be 3 (X, Y, Z).\\n")')
-    cg.close_c_block()
-    cg.blankline()
-
-    cg.write('// Make sure output length matches')
-    cg.write('size_t nsize')
-    cg.start_c_block('if (spherical)')
-    cg.write('    nsize = 2 * L + 1')
-    cg.write('} else {', endl="")
-    cg.write('    nsize = ((L + 2) * (L + 1)) / 2')
-    cg.close_c_block()
-    cg.blankline()
-
-    cg.start_c_block('if (out.shape(0) != nsize)')
-    cg.write('    throw std::length_error("Size of the output array does not match the angular momentum.\\n")')
-    cg.close_c_block()
-
-    for cart in deriv_indices:
-        cg.start_c_block('if (out_%s.shape(0) != nsize)' % cart)
-        cg.write('    throw std::length_error("Size of the output %s array does not match the angular momentum.\\n")' %
-                 cart.upper())
-        cg.close_c_block()
-    cg.blankline()
-
-    cg.write('// Ensure lengths match')
-    cg.start_c_block('if (out.shape(1) != arr_xyz.shape(1))')
-    cg.write('    throw std::length_error("Size of the output array and XYZ array must be the same.\\n")')
-    cg.close_c_block()
-
-    # Pad out deriv length checkers
-    for cart in deriv_indices:
-        cg.start_c_block('if (out_%s.shape(1) != arr_xyz.shape(1))' % cart)
-        cg.write('    throw std::length_error("Size of the output %s array and XYZ array must be the same.\\n")' %
-                 cart.upper())
-        cg.close_c_block()
-    cg.blankline()
-
-    cg.write("// Call the GG helper function")
-    call_func = call_name + "(L, xyz.shape(1)"
-    call_func += ", xyz.data(0, 0), xyz.data(1, 0), xyz.data(2, 0)"
-    call_func += ", coeffs.shape(0), coeffs.data(0), exponents.data(0)"
-    call_func += ", center.data(0)"
-    call_func += ", spherical"
-    call_func += ", out.mutable_data(0, 0)"
-    for cart in deriv_indices:
-        call_func += ", out_%s.mutable_data(0, 0)" % cart
-    call_func += ")"
-
-    cg.write(call_func)
-
-    cg.close_c_block()
-
-
 def _tmp_to_out_copy(cg, L, deriv_indices, inner_block):
 
     # Start spherical switch
@@ -913,170 +808,4 @@ def _tmp_to_out_copy(cg, L, deriv_indices, inner_block):
     # End spherical switch
     cg.close_c_block()
 
-def _pybind11_transpose(cg, func_name, wrapper_name):
 
-    sig = "void %s(py::array_t<double> arr_input" % wrapper_name
-    sig += ", py::array_t<double> arr_output)"
-
-    cg.start_c_block(sig)
-    cg.write("auto input = arr_input.unchecked<2>()")
-    cg.write("auto output = arr_output.mutable_unchecked<2>()")
-    cg.write("size_t n = input.shape(0)")
-    cg.write("size_t m = input.shape(1)")
-    cg.blankline()
-
-    cg.write('// Check shapes')
-    cg.start_c_block('if (input.shape(0) != output.shape(1))')
-    cg.write('    throw std::length_error("Input tranpose shape 0 does not match output transpose shape 1.\\n")')
-    cg.close_c_block()
-    cg.blankline()
-
-    cg.start_c_block('if (input.shape(1) != output.shape(0))')
-    cg.write('    throw std::length_error("Input tranpose shape 1 does not match output transpose shape 0.\\n")')
-    cg.close_c_block()
-    cg.blankline()
-
-    cg.write("%s(n, m, input.data(0, 0), output.mutable_data(0, 0))" % func_name)
-
-    cg.close_c_block()
-
-def _naive_transpose(cg):
-
-    sig = "void gg_naive_transpose(size_t n, size_t m, const double* __restrict__ input, double* __restrict__ output)"
-    cg.start_c_block(sig)
-
-    cg.start_c_block("for (size_t i = 0; i < n; i++)")
-
-    # Inner block
-    cg.start_c_block("for (size_t j = 0; j < m; j++)")
-    cg.write("output[j * n + i] = input[i * m + j]")
-    cg.close_c_block()
-
-    # Outer block
-    cg.close_c_block()
-
-    cg.close_c_block()
-    return sig
-
-
-def _fast_transpose(cg, inner_block):
-    """
-    Builds a fast transpose
-    """
-
-    sig = "void gg_fast_transpose(size_t n, size_t m, const double* __restrict__ input, double* __restrict__ output)"
-    cg.start_c_block(sig)
-    cg.blankline()
-
-    cg.write("// Temps")
-    cg.write("double tmp[%d]  __attribute__((aligned(64)))" % (inner_block * inner_block))
-
-    cg.write("// Sizing")
-    cg.write("size_t nblocks = n / %d" % inner_block)
-    cg.write("nblocks += (n %% %d) ? 1 : 0" % inner_block)
-
-    cg.write("size_t mblocks = m / %d" % inner_block)
-    cg.write("mblocks += (m %% %d) ? 1 : 0" % inner_block)
-    # cg.write('printf("Blocks: %ld %ld\\n", nblocks, mblocks)')
-
-
-    cg.write("// Outer blocks")
-    cg.start_c_block("for (size_t nb = 0; nb < nblocks; nb++)")
-    cg.write("const size_t nstart = nb * %d" % inner_block)
-    cg.write("size_t nremain = ((nstart + %d) > n) ? (n - nstart) : %d" % (inner_block, inner_block))
-
-    cg.start_c_block("for (size_t mb = 0; mb < mblocks; mb++)")
-    cg.write("const size_t mstart = mb * %d" % inner_block)
-    cg.write("size_t mremain = ((mstart + %d) > m) ? (m - mstart) : %d" % (inner_block, inner_block))
-
-    # cg.start_c_block("if ((nremain == 0) & (mremain > 0))")
-    # cg.write("nremain++;")
-    # cg.close_c_block()
-
-    # cg.start_c_block("if ((mremain == 0) & (nremain > 0))")
-    # cg.write("mremain++;")
-    # cg.close_c_block()
-    # cg.write('printf("(n,m)%ld %ld | %ld %ld\\n", nb, mb, nremain, mremain)')
-
-
-    # Pull block
-    cg.write("// Copy data to inner block")
-    # cg.write('printf("%ld %ld | %ld\\n ", mstart, nstart, start)')
-    cg.start_c_block("for (size_t l = 0; l < nremain; l++)")
-    cg.write("const size_t start = (nstart + l) * m + mstart")
-    cg.write("PRAGMA_VECTORIZE", endl="")
-    cg.start_c_block("for (size_t k = 0; k < mremain; k++)")
-
-    # cg.write("tmp[l * %d + k] = input[start + k]" % inner_block)
-    cg.write("tmp[k * %d + l] = input[start + k]" % inner_block)
-
-    # cg.write('printf("(%ld %ld %lf) ", l * 2+ k, start +k, input[start + k])')
-    # cg.write('printf("%%lf ", tmp[k * %d + l])' % inner_block)
-    cg.close_c_block()
-    cg.close_c_block()
-    # cg.write('printf("\\n--\\n")')
-    # cg.start_c_block("for (size_t k = 0; k < 4; k++)")
-    # cg.write('printf("%lf ", tmp[k])')
-    # cg.close_c_block()
-    # cg.write('printf("\\n--\\n")')
-
-    # Tranpose block
-    # cg.write("// Transpose inner block")
-    # cg.start_c_block("for (size_t k = 0; k < %d; k++)" % inner_block)
-    # cg.start_c_block("for (size_t l = k; l < %d; l++)" % inner_block)
-    # # cg.write('printf("%ld %ld \\n", k, l)')
-    # cg.write("const double itmp = tmp[l * %d + k]" % inner_block)
-    # cg.write("tmp[l * %d + k] = tmp[k * %d + l]" % (inner_block, inner_block))
-    # cg.write("tmp[k * %d + l] = itmp" % (inner_block))
-    # cg.close_c_block()
-    # cg.close_c_block()
-    # cg.write('printf("--\\n")')
-    # cg.start_c_block("for (size_t k = 0; k < 4; k++)")
-    # cg.write('printf("%lf ", tmp[k])')
-    # cg.close_c_block()
-    # cg.write('printf("\\n--\\n")')
-
-
-    # Push block
-    cg.write("// Copy data to inner block")
-    cg.start_c_block("for (size_t k = 0; k < mremain; k++)")
-    cg.write("const size_t start = (mstart + k) * n + nstart")
-    cg.write("PRAGMA_VECTORIZE", endl="")
-    cg.start_c_block("for (size_t l = 0; l < nremain; l++)")
-    # cg.write('printf("(k,l) %ld %ld | %ld\\n", k, l, start+l)')
-
-    cg.write("output[start + l] = tmp[k * %d + l]" % inner_block)
-    cg.close_c_block()
-    cg.close_c_block()
-    # cg.write('printf("--------\\n")')
-
-    # cg.start_c_block("for (size_t k = 0; k < %d; k++)" % inner_block)
-    # cg.start_c_block("for (size_t l = 0; l < %d; l++)" % inner_block)
-    # cg.write("tmp[k * %d + l] = 0.0" % inner_block)
-    # cg.close_c_block()
-    # cg.close_c_block()
-
-
-    # Outer block
-    cg.close_c_block()
-    cg.close_c_block()
-
-    cg.close_c_block()
-
-    return sig
-
-
-
-def generate_hello(path='.'):
-    print(path)
-    with open(path + '/hello.c', 'w') as fl:
-        fl.write("""
-/* Hello World program */
-
-#include<stdio.h>
-
-int main()
-{
-    printf("Hello World");
-}
-""")
---
+++ gau2grid/c_util_generator.py
@@ -0,0 +1,270 @@
+"""
+Builds c utility routines
+"""
+
+from . import utility
+
+### Pybind11 binders
+
+
+def pybind11_func(cg, name, grad, call_name, max_L):
+    """
+    A function that builds the PyBind11 wrapper functions
+    """
+
+    # Figure out what we need to add per deriv
+    deriv_indices = utility.get_deriv_indices(grad)
+
+    # Write out wrapper functions
+    sig = """void %s(int L, py::array_t<double> arr_xyz, py::array_t<double> arr_coeffs,
+py::array_t<double> arr_exponents, py::array_t<double> arr_center, bool spherical,
+py::array_t<double> arr_out""" % name
+
+    # Pad out deriv outputs
+    for cart in deriv_indices:
+        sig += ", py::array_t<double> arr_%s_out" % cart
+
+    sig += ")"
+    cg.start_c_block(sig)
+    cg.blankline()
+
+    # Grab the pointers
+    cg.write('// Grab array pointers')
+    cg.write('auto xyz = arr_xyz.unchecked<2>()')
+    cg.write('auto coeffs = arr_coeffs.unchecked<1>()')
+    cg.write('auto exponents = arr_exponents.unchecked<1>()')
+    cg.write('auto center = arr_center.unchecked<1>()')
+    cg.write('auto out = arr_out.mutable_unchecked<2>()')
+
+    # Pad out deriv pointers
+    for cart in deriv_indices:
+        cg.write("auto out_%s = arr_%s_out.mutable_unchecked<2>()" % (cart, cart))
+
+    cg.blankline()
+
+    # Run through checks
+    cg.write('// XYZ is of size 3')
+    cg.start_c_block('if (L > %d)' % max_L)
+    cg.write(
+        '    throw std::invalid_argument("Exceeded compiled angular momentum of %d. Please recompile with a higher angular momentum.\\n")'
+        % max_L)
+    cg.close_c_block()
+
+    cg.write('// XYZ is of size 3')
+    cg.start_c_block('if (arr_xyz.shape(0) != 3)')
+    cg.write('    throw std::length_error("Length of XYZ array must be (3, n).\\n")')
+    cg.close_c_block()
+    cg.blankline()
+
+    cg.write('// Coeff matches exponent shape')
+    cg.start_c_block('if (coeffs.shape(0) != exponents.shape(0))')
+    cg.write('    throw std::length_error("Length of coefficients and exponents must match.\\n")')
+    cg.close_c_block()
+    cg.blankline()
+
+    cg.write('// Center is of size 3')
+    cg.start_c_block('if (center.shape(0) != 3)')
+    cg.write('    throw std::length_error("Length of center vector must be 3 (X, Y, Z).\\n")')
+    cg.close_c_block()
+    cg.blankline()
+
+    cg.write('// Make sure output length matches')
+    cg.write('size_t nsize')
+    cg.start_c_block('if (spherical)')
+    cg.write('    nsize = 2 * L + 1')
+    cg.write('} else {', endl="")
+    cg.write('    nsize = ((L + 2) * (L + 1)) / 2')
+    cg.close_c_block()
+    cg.blankline()
+
+    cg.start_c_block('if (out.shape(0) != nsize)')
+    cg.write('    throw std::length_error("Size of the output array does not match the angular momentum.\\n")')
+    cg.close_c_block()
+
+    for cart in deriv_indices:
+        cg.start_c_block('if (out_%s.shape(0) != nsize)' % cart)
+        cg.write('    throw std::length_error("Size of the output %s array does not match the angular momentum.\\n")' %
+                 cart.upper())
+        cg.close_c_block()
+    cg.blankline()
+
+    cg.write('// Ensure lengths match')
+    cg.start_c_block('if (out.shape(1) != arr_xyz.shape(1))')
+    cg.write('    throw std::length_error("Size of the output array and XYZ array must be the same.\\n")')
+    cg.close_c_block()
+
+    # Pad out deriv length checkers
+    for cart in deriv_indices:
+        cg.start_c_block('if (out_%s.shape(1) != arr_xyz.shape(1))' % cart)
+        cg.write('    throw std::length_error("Size of the output %s array and XYZ array must be the same.\\n")' %
+                 cart.upper())
+        cg.close_c_block()
+    cg.blankline()
+
+    cg.write("// Call the GG helper function")
+    call_func = call_name + "(L, xyz.shape(1)"
+    call_func += ", xyz.data(0, 0), xyz.data(1, 0), xyz.data(2, 0)"
+    call_func += ", coeffs.shape(0), coeffs.data(0), exponents.data(0)"
+    call_func += ", center.data(0)"
+    call_func += ", spherical"
+    call_func += ", out.mutable_data(0, 0)"
+    for cart in deriv_indices:
+        call_func += ", out_%s.mutable_data(0, 0)" % cart
+    call_func += ")"
+
+    cg.write(call_func)
+
+    cg.close_c_block()
+
+
+def pybind11_transpose(cg, func_name, wrapper_name):
+
+    sig = "void %s(py::array_t<double> arr_input" % wrapper_name
+    sig += ", py::array_t<double> arr_output)"
+
+    cg.start_c_block(sig)
+    cg.write("auto input = arr_input.unchecked<2>()")
+    cg.write("auto output = arr_output.mutable_unchecked<2>()")
+    cg.write("size_t n = input.shape(0)")
+    cg.write("size_t m = input.shape(1)")
+    cg.blankline()
+
+    cg.write('// Check shapes')
+    cg.start_c_block('if (input.shape(0) != output.shape(1))')
+    cg.write('    throw std::length_error("Input tranpose shape 0 does not match output transpose shape 1.\\n")')
+    cg.close_c_block()
+    cg.blankline()
+
+    cg.start_c_block('if (input.shape(1) != output.shape(0))')
+    cg.write('    throw std::length_error("Input tranpose shape 1 does not match output transpose shape 0.\\n")')
+    cg.close_c_block()
+    cg.blankline()
+
+    cg.write("%s(n, m, input.data(0, 0), output.mutable_data(0, 0))" % func_name)
+
+    cg.close_c_block()
+
+
+### Tranposers
+
+
+def naive_transpose(cg):
+
+    sig = "void gg_naive_transpose(size_t n, size_t m, const double* __restrict__ input, double* __restrict__ output)"
+    cg.start_c_block(sig)
+
+    cg.start_c_block("for (size_t i = 0; i < n; i++)")
+
+    # Inner block
+    cg.start_c_block("for (size_t j = 0; j < m; j++)")
+    cg.write("output[j * n + i] = input[i * m + j]")
+    cg.close_c_block()
+
+    # Outer block
+    cg.close_c_block()
+
+    cg.close_c_block()
+    return sig
+
+
+def fast_transpose(cg, inner_block):
+    """
+    Builds a fast transpose
+    """
+
+    sig = "void gg_fast_transpose(size_t n, size_t m, const double* __restrict__ input, double* __restrict__ output)"
+    cg.start_c_block(sig)
+    cg.blankline()
+
+    cg.write("// Temps")
+    cg.write("double tmp[%d]  __attribute__((aligned(64)))" % (inner_block * inner_block))
+
+    cg.write("// Sizing")
+    cg.write("size_t nblocks = n / %d" % inner_block)
+    cg.write("nblocks += (n %% %d) ? 1 : 0" % inner_block)
+
+    cg.write("size_t mblocks = m / %d" % inner_block)
+    cg.write("mblocks += (m %% %d) ? 1 : 0" % inner_block)
+    # cg.write('printf("Blocks: %ld %ld\\n", nblocks, mblocks)')
+
+    cg.write("// Outer blocks")
+    cg.start_c_block("for (size_t nb = 0; nb < nblocks; nb++)")
+    cg.write("const size_t nstart = nb * %d" % inner_block)
+    cg.write("size_t nremain = ((nstart + %d) > n) ? (n - nstart) : %d" % (inner_block, inner_block))
+
+    cg.start_c_block("for (size_t mb = 0; mb < mblocks; mb++)")
+    cg.write("const size_t mstart = mb * %d" % inner_block)
+    cg.write("size_t mremain = ((mstart + %d) > m) ? (m - mstart) : %d" % (inner_block, inner_block))
+
+    # cg.start_c_block("if ((nremain == 0) & (mremain > 0))")
+    # cg.write("nremain++;")
+    # cg.close_c_block()
+
+    # cg.start_c_block("if ((mremain == 0) & (nremain > 0))")
+    # cg.write("mremain++;")
+    # cg.close_c_block()
+    # cg.write('printf("(n,m)%ld %ld | %ld %ld\\n", nb, mb, nremain, mremain)')
+
+    # Pull block
+    cg.write("// Copy data to inner block")
+    # cg.write('printf("%ld %ld | %ld\\n ", mstart, nstart, start)')
+    cg.start_c_block("for (size_t l = 0; l < nremain; l++)")
+    cg.write("const size_t start = (nstart + l) * m + mstart")
+    cg.write("PRAGMA_VECTORIZE", endl="")
+    cg.start_c_block("for (size_t k = 0; k < mremain; k++)")
+
+    # cg.write("tmp[l * %d + k] = input[start + k]" % inner_block)
+    cg.write("tmp[k * %d + l] = input[start + k]" % inner_block)
+
+    # cg.write('printf("(%ld %ld %lf) ", l * 2+ k, start +k, input[start + k])')
+    # cg.write('printf("%%lf ", tmp[k * %d + l])' % inner_block)
+    cg.close_c_block()
+    cg.close_c_block()
+    # cg.write('printf("\\n--\\n")')
+    # cg.start_c_block("for (size_t k = 0; k < 4; k++)")
+    # cg.write('printf("%lf ", tmp[k])')
+    # cg.close_c_block()
+    # cg.write('printf("\\n--\\n")')
+
+    # Tranpose block
+    # cg.write("// Transpose inner block")
+    # cg.start_c_block("for (size_t k = 0; k < %d; k++)" % inner_block)
+    # cg.start_c_block("for (size_t l = k; l < %d; l++)" % inner_block)
+    # # cg.write('printf("%ld %ld \\n", k, l)')
+    # cg.write("const double itmp = tmp[l * %d + k]" % inner_block)
+    # cg.write("tmp[l * %d + k] = tmp[k * %d + l]" % (inner_block, inner_block))
+    # cg.write("tmp[k * %d + l] = itmp" % (inner_block))
+    # cg.close_c_block()
+    # cg.close_c_block()
+    # cg.write('printf("--\\n")')
+    # cg.start_c_block("for (size_t k = 0; k < 4; k++)")
+    # cg.write('printf("%lf ", tmp[k])')
+    # cg.close_c_block()
+    # cg.write('printf("\\n--\\n")')
+
+    # Push block
+    cg.write("// Copy data to inner block")
+    cg.start_c_block("for (size_t k = 0; k < mremain; k++)")
+    cg.write("const size_t start = (mstart + k) * n + nstart")
+    cg.write("PRAGMA_VECTORIZE", endl="")
+    cg.start_c_block("for (size_t l = 0; l < nremain; l++)")
+    # cg.write('printf("(k,l) %ld %ld | %ld\\n", k, l, start+l)')
+
+    cg.write("output[start + l] = tmp[k * %d + l]" % inner_block)
+    cg.close_c_block()
+    cg.close_c_block()
+    # cg.write('printf("--------\\n")')
+
+    # cg.start_c_block("for (size_t k = 0; k < %d; k++)" % inner_block)
+    # cg.start_c_block("for (size_t l = 0; l < %d; l++)" % inner_block)
+    # cg.write("tmp[k * %d + l] = 0.0" % inner_block)
+    # cg.close_c_block()
+    # cg.close_c_block()
+
+    # Outer block
+    cg.close_c_block()
+    cg.close_c_block()
+
+    cg.close_c_block()
+
+    return sig
