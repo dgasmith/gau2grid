eb684f15c3fb82d91f40e986dffe1ac7d8843957
Daniel Smith <malorian@me.com>
2018-04-01 14:43:41 -0400

    Adds versioneer

--- gau2grid/__init__.py
+++ gau2grid/__init__.py
@@ -11,3 +11,10 @@ from . import python_reference as ref
 
 # Pull in code from the c wrapper
 from .c_wrapper import collocation, collocation_basis, c_compiled, cgg_path
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
