72f1d5bfd182e648b403906a2844aff7f3a6411f
Daniel Smith <malorian@me.com>
2018-10-20 15:52:00 -0400

    PRAGMA: Moves mm_malloc to aligned_alloc where possible for great compat.

--- gau2grid/c_pragma.py
+++ gau2grid/c_pragma.py
@@ -7,35 +7,55 @@ _pragma_data = """
 
 #if defined(__ICC) || defined(__INTEL_COMPILER)
     // pragmas for Intel
+
+    #define ALIGNED_MALLOC(alignment, size)                  aligned_alloc(size, alignment)
+    #define ALIGNED_FREE(ptr)                                free(ptr)
+    #define ASSUME_ALIGNED(ptr, width)                       __assume_aligned(ptr, width)
+
     #define PRAGMA_VECTORIZE                                 _Pragma("vector")
     #define PRAGMA_RESTRICT                                  __restrict__
-    #define ASSUME_ALIGNED(ptr, width)                       __assume_aligned(ptr, width)
 
 #elif defined(__clang__)
     // pragmas for Clang.
     // Do this before GCC because clang also defines __GNUC__
+
+    #define ALIGNED_MALLOC(alignment, size)                  _mm_malloc(size, alignment)
+    #define ALIGNED_FREE(ptr)                                _mm_free(ptr)
+    #define ASSUME_ALIGNED(ptr, width)
+
     #define PRAGMA_VECTORIZE                                 _Pragma("clang loop vectorize(enable)")
     #define PRAGMA_RESTRICT                                  __restrict__
-    #define ASSUME_ALIGNED(ptr, width)
 
 #elif defined(__GNUC__) || defined(__GNUG__)
     // pragmas for GCC
+
+    #define ALIGNED_MALLOC(alignment, size)                  aligned_alloc(alignment, size)
+    #define ALIGNED_FREE(ptr)                                free(ptr)
+    #define ASSUME_ALIGNED(ptr, width)
+
     #define PRAGMA_VECTORIZE                                 _Pragma("GCC ivdep")
     #define PRAGMA_RESTRICT                                  __restrict__
-    #define ASSUME_ALIGNED(ptr, width)
 
 #elif defined(_MSC_VER)
     // pragmas for MSVC
+
+    #define ALIGNED_MALLOC(alignment, size)                  _aligned_malloc(size, alignment)
+    #define ALIGNED_FREE(ptr)                                _aligned_free(ptr)
+    #define ASSUME_ALIGNED(ptr, width)
+
     #define PRAGMA_VECTORIZE                                 __pragma(loop(ivdep))
     #define PRAGMA_RESTRICT                                  __restrict
-    #define ASSUME_ALIGNED(ptr, width)
 
 
 #elif defined(__PGI)
     // pragmas for PGI
+
+    #define ALIGNED_MALLOC(alignment, size)                  aligned_alloc(alignment, size)
+    #define ALIGNED_FREE(ptr)                                free(ptr)
+    #define ASSUME_ALIGNED(ptr, width)
+
     #define PRAGMA_VECTORIZE                                 _Pragma("ivdep")
     #define PRAGMA_RESTRICT                                  __restrict__
-    #define ASSUME_ALIGNED(ptr, width)
 
 
 #endif
